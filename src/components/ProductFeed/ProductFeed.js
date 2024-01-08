e91e6cf68c357c6b10e63d0dcb8a4572c87a348a
--global <DruSerkes@gmail.com>
2024-01-07 21:40:05 -0500

    bring the items up

--- src/components/ProductFeed/ProductFeed.js
+++ src/components/ProductFeed/ProductFeed.js
@@ -3,7 +3,7 @@ import { Product } from "./Product";
 export const ProductFeed = ({ products }) => {
   console.log({ products });
   return (
-    <div className="relative">
+    <div className="relative -mt-10 md:-mt-52 z-30">
       <div className="grid grid-flow-row-dense grid-cols-1 md:grid-cols-3 xl:grid-cols-4 ">
         {products.map((product) => (
           <Product key={product.id} {...product} />
